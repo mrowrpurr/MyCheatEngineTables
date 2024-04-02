e85e51fb9c7ccbf6de0e2a660a6d274d81056dc2
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-01 19:13:21 -0700

    Ctrl+Shift+N to Print CNpc Infos

--- Divine Divinity/EntityList.lua
+++ Divine Divinity/EntityList.lua
@@ -6,8 +6,10 @@ function getEntityListAddress()
   return getAddy(VAR_ENTITY_LIST_PTR)
 end
 
-function getEntityPointer(offset)
-  local address = getEntityListAddress() + offset
-  print("Reading Entity Pointer: " .. hex(address))
-  return readPointer(address)
-end
+-- function readEntityPointer(offset)
+--   if offset then
+--     return readPointer(getEntityListAddress() + offset)
+--   else
+--     return nil
+--   end
+-- end
--- Divine Divinity/Print_CNpc_Info.lua
+++ Divine Divinity/Print_CNpc_Info.lua
@@ -7,15 +7,31 @@ function getFirstCNpcOffset()
   return getAddy(VAR_FIRST_CNPC_OFFSET)
 end
 
+function printEntityInfo(entityAddress)
+  print("Printing entity located at " .. hex(entityAddress))
+end
+
 function Print_CNpc_Info_Enable()
   local entityListAddress = getEntityListAddress()
-  print("Entity List Pointer: " .. hex(entityListAddress))
+  local firstOffset = getFirstCNpcOffset()
+  local offsetDistance = 0x4
 
-  local offset = getFirstCNpcOffset()
-  print("First CNpc Offset: " .. hex(offset))
+  local currentEntityPointerAddress = entityListAddress + firstOffset
+  print("First CNpc entity pointer address: " .. hex(currentEntityPointerAddress))
 
-  local firstEntityPointer = getEntityPointer(getFirstCNpcOffset())
-  print("First CNpc Pointer: " .. hex(firstEntityPointer))
+  local currentEntityAddress = readPointer(currentEntityPointerAddress)
+  print("First CNpc entity address: " .. hex(currentEntityAddress))
+
+  local entityCount = 0
+
+  while entityCount < 10 and currentEntityPointerAddress > 0 and currentEntityAddress > 0 do
+    printEntityInfo(currentEntityAddress)
+    currentEntityPointerAddress = currentEntityPointerAddress + offsetDistance
+    currentEntityAddress = readPointer(currentEntityPointerAddress)
+    entityCount = entityCount + 1
+  end
+
+  print("Total CNpc entities: " .. entityCount)
 end
 
 function Print_CNpc_Info_Disable()
