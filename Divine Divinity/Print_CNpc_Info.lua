f1be1c31e78cf6941d04d5c8ab06575f705109e6
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-01 19:23:48 -0700

    Level MStats+1c, set HP to entity index in script

--- Divine Divinity/Print_CNpc_Info.lua
+++ Divine Divinity/Print_CNpc_Info.lua
@@ -7,28 +7,41 @@ function getFirstCNpcOffset()
   return getAddy(VAR_FIRST_CNPC_OFFSET)
 end
 
-function printEntityInfo(entityAddress)
-  print("Printing entity located at " .. hex(entityAddress))
+function printMonsterStatsForEntity(entityIndex, entityAddress)
+  local monsterStatsAddress = entityAddress + 0x2c
+  local monsterStats = readPointer(monsterStatsAddress)
+  writeInteger(monsterStats + 0x4, entityIndex)
+  local hp = readInteger(monsterStats + 0x4)
+  print("> HP: " .. hp)
+  local level = readInteger(monsterStats + 0x1c)
+  print("> Level: " .. level)
+end
+
+function printEntityInfo(entityIndex, entityAddress)
+  print("PRINT ENTITY @ " .. hex(entityAddress))
+  local possibleIdentifier1 = readInteger(entityAddress + 0x1c)
+  print("> ID1: " .. possibleIdentifier1)
+  local possibleIdentifier2 = readInteger(entityAddress + 0x20)
+  print("> ID2: " .. possibleIdentifier2)
+  local possibleIdentifier3 = readInteger(entityAddress + 0x80)
+  print("> ID3: " .. possibleIdentifier3)
+  printMonsterStatsForEntity(entityIndex, entityAddress)
+  print("")
 end
 
 function Print_CNpc_Info_Enable()
   local entityListAddress = getEntityListAddress()
   local firstOffset = getFirstCNpcOffset()
   local offsetDistance = 0x4
-
   local currentEntityPointerAddress = entityListAddress + firstOffset
-  print("First CNpc entity pointer address: " .. hex(currentEntityPointerAddress))
-
   local currentEntityAddress = readPointer(currentEntityPointerAddress)
-  print("First CNpc entity address: " .. hex(currentEntityAddress))
-
   local entityCount = 0
 
   while entityCount < 10 and currentEntityPointerAddress > 0 and currentEntityAddress > 0 do
-    printEntityInfo(currentEntityAddress)
+    entityCount = entityCount + 1
+    printEntityInfo(entityCount, currentEntityAddress)
     currentEntityPointerAddress = currentEntityPointerAddress + offsetDistance
     currentEntityAddress = readPointer(currentEntityPointerAddress)
-    entityCount = entityCount + 1
   end
 
   print("Total CNpc entities: " .. entityCount)
