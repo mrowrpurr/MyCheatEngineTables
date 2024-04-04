74b999f81a63753cb67b2422fa5ae9b00b05bf5b
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-03 21:53:41 -0700

    requireScript(...)

--- Code/common.ts
+++ Code/common.ts
@@ -1,18 +1,3 @@
-const ALL_SCRIPTS_PREFIX = "Code/"
-
-export function reloadScript(fullPackageName: string) {
-    _G["package"]["loaded"][fullPackageName] = undefined
-}
-
-export function reloadScripts(...prefixes: string[]) {
-    for (const key in _G["package"]["loaded"])
-        if (prefixes.some((prefix) => key.startsWith(prefix))) reloadScript(key)
-}
-
-export function reloadAllScripts() {
-    reloadScripts(ALL_SCRIPTS_PREFIX)
-}
-
 export function hex(int: number) {
     return int.toString(16)
 }
