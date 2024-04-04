13855b109069ae5b78712847beb66f0bdb19a110
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-03 21:53:50 -0700

    Script to print top of the stack!

--- Code/Debugging/PrintTopOfStack.ts
+++ Code/Debugging/PrintTopOfStack.ts
@@ -1,10 +1,43 @@
-import { getDebugger } from "@cheat-engine"
+import { clearLogWindow } from "@cheat-engine"
+
+export function printAddress(address: number) {
+    const addressString = address.toString(16).toUpperCase().padStart(8, "0")
+    const isSafe = getAddressSafe(address) !== undefined
+    const asInteger = isSafe ? readInteger(address) : undefined
+    let asFloat = isSafe ? readFloat(address) : undefined
+    if (asFloat?.toString().includes("e")) asFloat = undefined
+    const asPointer = asInteger && getAddressSafe(asInteger) ? readPointer(asInteger) : undefined
+    const pointerAddressString =
+        asPointer === undefined ? undefined : asPointer.toString(16).toUpperCase().padStart(8, "0")
+
+    // TODO: don't show float if it has an E in it
+
+    let output = `${addressString}`
+    if (asInteger !== undefined) {
+        if (asPointer !== undefined) {
+            output += ` \t* ${pointerAddressString}`
+            output += ` \t[${asInteger}]`
+            if (asFloat !== undefined) output += ` \t(${asFloat})`
+        } else {
+            output += `: \t${asInteger}`
+            if (asFloat !== undefined) output += ` \t(${asFloat})`
+        }
+    }
+
+    print(output)
+
+    return asInteger !== undefined
+}
+
+export function printTopOfStack(items: number = 10) {
+    if (ESP === undefined) return
+    print(`Top of stack: ESP = ${ESP.toString(16).toUpperCase().padStart(8, "0")}`)
+    for (let i = 0; i < items; i++) if (!printAddress(ESP + i * 0x4)) break
+}
 
 export function enable() {
-    const debug = getDebugger()
-    if (debug.isDebugging()) {
-        print("Is Debugging!")
-    }
+    clearLogWindow()
+    printTopOfStack()
 }
 
 export function disable() {}
