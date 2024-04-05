32670d965a03be34c4ff2d00f12d13abc73a0b21
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-04 21:52:37 -0700

    getAddressString, readSimpleFloat

--- Code/common.ts
+++ Code/common.ts
@@ -7,3 +7,14 @@ export function getAddress(addressString: string) {
     if (address === undefined) print(`Address not found: ${addressString}`)
     return address?.CurrentAddress
 }
+
+export function getAddressString(address: number, padding: number = 8) {
+    return address.toString(16).toUpperCase().padStart(padding, "0")
+}
+
+export function readSimpleFloat(address: number) {
+    const floatString = readFloat(address).toString()
+    if (floatString.includes("e")) return undefined
+    if (floatString.includes("nan")) return undefined
+    return floatString
+}
--- Lua/Code/common.lua
+++ Lua/Code/common.lua
@@ -1,5 +1,7 @@
 local ____lualib = require("lualib_bundle")
 local __TS__NumberToString = ____lualib.__TS__NumberToString
+local __TS__StringPadStart = ____lualib.__TS__StringPadStart
+local __TS__StringIncludes = ____lualib.__TS__StringIncludes
 local ____exports = {}
 function ____exports.hex(int)
     return __TS__NumberToString(int, 16)
@@ -10,5 +12,25 @@ function ____exports.getAddress(addressString)
         print("Address not found: " .. addressString)
     end
     return address and address.CurrentAddress
+end
+function ____exports.getAddressString(address, padding)
+    if padding == nil then
+        padding = 8
+    end
+    return __TS__StringPadStart(
+        string.upper(__TS__NumberToString(address, 16)),
+        padding,
+        "0"
+    )
+end
+function ____exports.readSimpleFloat(address)
+    local floatString = tostring(readFloat(address))
+    if __TS__StringIncludes(floatString, "e") then
+        return nil
+    end
+    if __TS__StringIncludes(floatString, "nan") then
+        return nil
+    end
+    return floatString
 end
 return ____exports
