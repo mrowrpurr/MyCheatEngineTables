9f62135be045035d53363a8241404f84bfdf5b1c
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-06 13:15:48 -0700

    Delete and ignore the .lua and .lua.map files

--- Lua/Code/Examples/HelloWorld.lua
+++
@@ -1,9 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-function ____exports.enable()
-    print("Hello, world!")
-end
-function ____exports.disable()
-    print("Goodnight, moon!")
-end
-return ____exports
