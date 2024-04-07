5ef324f18c49d4e403ac009a7fef86d8526283f8
Mrowr Purr <mrowr.purr@gmail.com>
2024-04-06 22:20:46 -0700

    Hello, world CE C++ plugin

---
+++ Plugins/Example Plugin/example_ce_plugin.cpp
@@ -0,0 +1,35 @@
+#include <_Log_.h>
+#include <cepluginsdk.h>
+
+#include <string>
+#include <vector>
+
+_LogToFile_("C:/Temp/CEPlugin_HelloWorld.log");
+
+const std::string PLUGIN_NAME = "Hello, plugin";
+
+extern "C" BOOL __declspec(dllexport) CEPlugin_GetVersion(PPluginVersion pluginVersion, int sizeofpluginversion) {
+    _Log_("CEPlugin_GetVersion!");
+
+    std::vector<char> pluginName(PLUGIN_NAME.begin(), PLUGIN_NAME.end());
+    pluginName.push_back('\0');
+
+    pluginVersion->version    = CESDK_VERSION;
+    pluginVersion->pluginname = pluginName.data();
+
+    return TRUE;
+}
+
+extern "C" BOOL __declspec(dllexport) CEPlugin_InitializePlugin(PExportedFunctions exportedFunctions, int pluginId) {
+    _Log_("CEPlugin_InitializePlugin!");
+
+    // exportedFunctions->ShowMessage("Hello, CEPlugin!");
+
+    return TRUE;
+}
+
+extern "C" BOOL __declspec(dllexport) CEPlugin_DisablePlugin() {
+    _Log_("CEPlugin_DisablePlugin!");
+
+    return TRUE;
+}
